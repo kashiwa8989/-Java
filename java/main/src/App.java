42487e5343e923ec8b88f7b02503e6575e91068e
kashiwa8989 <119640627+kashiwa8989@users.noreply.github.com>
2023-03-14 20:46:17 +0900

    Progate_paractce1

--- java/main/src/App.java
+++ java/main/src/App.java
@@ -1,5 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("初めてのJava");
+        System.out.println(17);
+        System.out.println(5+3);
+        System.out.println("5+3");
+        System.out.println(12/3);
+        System.out.println(3*6);
+        System.out.println(8%3);
+        System.out.println("こんにちは"+"世界");
+        System.out.println("38"+"19");
+        System.out.println(38+19);
+
+        int number;
+        number = 3;
+        System.out.println(number);
+
+        String name;
+        name ="Wanko";
+        System.out.println(name);
+
     }
 }
