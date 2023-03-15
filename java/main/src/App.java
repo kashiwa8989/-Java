da12f51cb6e95ad50990ffb381d497a17a450169
kashiwa8989 <119640627+kashiwa8989@users.noreply.github.com>
2023-03-15 22:33:41 +0900

    演習2

--- java/main/src/App.java
+++ java/main/src/App.java
@@ -12,13 +12,47 @@ public class App {
         System.out.println("38"+"19");
         System.out.println(38+19);
 
-        int number;
-        number = 3;
-        System.out.println(number);
+        int number0;
+        number0 = 3;
+        System.out.println(number0);
 
         String name;
         name ="Wanko";
         System.out.println(name);
+
+        int number1 = 3;
+        System.out.println(number1);
+        int number2 = 7;
+        System.out.println(number2);
+        System.out.println(number1*number2);
+
+        String text0 = "プログラミングを勉強しよう";
+        System.out.println("Progateで"+text0);
+
+        //変数の更新
+        int number = 11;
+        String text = "Ruby";
+        System.out.println(number);
+        System.out.println(text);
+
+        number = 9;
+        System.out.println(number);
+        text = "Java";
+        System.out.println(text);
+
+        //自己代入
+        int number4 = 3;
+        System.out.println(number4);
+        number4 = number4+7;
+        System.out.println(number4);
+
+        //自己代入の省略した書き方
+        
+
+
+
+
+        
 
     }
 }
