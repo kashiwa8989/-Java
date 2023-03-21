771a7dd9d006124350e9620a0a5b117c83701c49
kashiwa8989 <119640627+kashiwa8989@users.noreply.github.com>
2023-03-21 23:44:54 +0900

    演習7

--- java/main/src/Practice.java
+++ java/main/src/Practice.java
@@ -83,6 +83,27 @@ public class  Practice {
             System.out.println(i+"回目のループです");
         }
 
+        //while文　continue break
+        System.out.println("=== while文 ===");
+        int i = 1;
+        while (i < 10) {
+        // iが5の倍数のとき、繰り返し処理を終了
+        if (i % 5 ==0){
+            break;
+        }
+        System.out.println(i);
+        i++;
+        }
+        
+        System.out.println("=== for文 ===");
+        for (int j = 1; j < 10; j++) {
+        // jが3の倍数のとき、処理をスキップ
+        if (j % 3 == 0){
+            continue;
+        }
+        System.out.println(j);
+        }
+
 
         
 
