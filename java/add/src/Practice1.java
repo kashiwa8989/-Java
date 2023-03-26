f63ab66a7500799bb12cb437aed380b411ffdc4a
kashiwa8989 <119640627+kashiwa8989@users.noreply.github.com>
2023-03-26 22:22:04 +0900

    演習9

---
+++ java/add/src/Practice1.java
@@ -0,0 +1,9 @@
+public class Practice1 {
+    public static void main(String[] args) throws Exception {
+        //メソッド1
+        hello();
+    }
+    public static void hello(){
+        System.out.println("Hello Java");
+    }
+}
--- java/main/src/Practice3.java
+++ java/main/src/Practice3.java
@@ -2,10 +2,15 @@ public class Practice3 {
     public static void main(String[] args) throws Exception {
     //メソッド1
         hello();
+        printData();
     }
     public static void hello(){
         System.out.println("Hello Java");
     }
+    public static void printData(){
+        System.out.println("私の名前はKate Jonesです");
+    }
+
 
 
 
