abee59ce6e4c1fa742b9de98d1ad6795eb9387e9
kashiwa8989 <119640627+kashiwa8989@users.noreply.github.com>
2023-03-27 23:22:26 +0900

    演習10

--- java/main/src/Practice3.java
+++ java/main/src/Practice3.java
@@ -2,14 +2,17 @@ public class Practice3 {
     public static void main(String[] args) throws Exception {
     //メソッド1
         hello();
-        printData();
+        printData("Kate Jones",27);
+        printData("John Christopher Smith",65);
     }
     public static void hello(){
         System.out.println("Hello Java");
     }
-    public static void printData(){
-        System.out.println("私の名前はKate Jonesです");
+    public static void printData(String name,int age){
+        System.out.println("私の名前は"+name+"です");
+        System.out.println("年齢は"+age+"歳です");
     }
+
 
 
 
